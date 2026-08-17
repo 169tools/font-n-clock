1935de0022fd586218231df387efc0828f9d000a
bookii <bookii.swift@gmail.com>
2026-08-18 06:31:55 +0900

    include の修正

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -17,8 +17,9 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
 #include <obs-module.h>
-#include <plugin-support.h>
+#include <util/base.h>
 
+#include "plugin-support.h"
 #include "color-source.hpp"
 
 OBS_DECLARE_MODULE()
