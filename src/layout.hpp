0d3b550c4e97b1fe586f6947586a601f73ab2d88
bookii <bookii.swift@gmail.com>
2026-08-19 22:14:34 +0900

    staticなテキストを描画する

---
+++ src/layout.hpp
@@ -0,0 +1,28 @@
+/*
+font-meets-clock
+Copyright (C) 2026 mizznoff <mizznoff@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#pragma once
+
+struct ink_extents {
+	double width = 0.0;
+	double ascent = 0.0;
+	double descent = 0.0;
+	double left = 0.0;
+
+	double height() const { return ascent + descent; }
+};
